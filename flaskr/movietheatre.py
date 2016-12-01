094c2b80356483284743175f7cac3de06ce7901e
Angel <lren22@uwo.ca>
2016-12-01 00:29:03 -0500

    add movie functions now

--- flaskr/movietheatre.py
+++ flaskr/movietheatre.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
 import mysql.connector
+from mysql.connector.constants import ClientFlag
+
+config = {
+    'user': 'root',
+    'password': 'love',
+    'database': 'MovieTheatre'
+}
 
 app = Flask(__name__)
 
@@ -12,30 +19,37 @@ def main():
 def backend():
     return render_template('backend.html')
 
-#front end
+# front end
 @app.route("/frontend")
 def frontend():
     return render_template('frontend.html')
 
+# movie
+@app.route("/movie")
+def moivepage():
+    return render_template('movie.html')
+
+@app.route("/addmovie")
+def addmoivepage():
+    return render_template('addmovie.html')
+
 # add a movie (ID, name, year)
 @app.route("/submit", methods=["POST"])
 def addMovie():
-    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
     insert_stmt = (
-        "INSERT INTO `Movie` VALUES (idMovie,MovieName,MovieYear)"
-        "VALUES (%d, %s, %d)"
+        "INSERT INTO `Movie` (idMovie,MovieName,MovieYear)"
+        "VALUES (%s, %s, %s)"
         )
     data = (request.form['idMovie'], request.form['MovieName'], request.form['MovieYear'])
     cursor.execute(insert_stmt, data)
+    cnx.commit()
     cnx.close()
     return render_template('addmovie.html', idMovie=request.form['idMovie'], 
         MovieName=request.form['MovieName'], MovieYear=request.form['MovieYear'])
 
 # delete a movie by name
-def deleteMovie():
-    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
-    cursor = cnx.cursor()
 
 
 if __name__ == "__main__":
